a5d936c5a5651eac820fd4665a8ffac1ab78836b
omar-tarek-abdelfattah <omartarek77124@gmail.com>
2026-06-30 19:09:02 +0300

    fixed authentication

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,34 +1,23 @@
 import { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
-import User, { HUserDocument } from "../DB/models/User.model";
+import { HUserDocument } from "../DB/models/User.model";
 import { UnauthorizedException } from "../utils/response/error.response";
-import { UserRepository } from "../DB/repositories/user.repository";
-
-const JWT_SECRET = process.env.JWT_SECRET || "default_random_secret_12345";
-const userModel = new UserRepository(User)
+import { decodeToken, TokenEnum } from "../utils/security/token.security";
 
 export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const authHeader = req.headers.authorization;
-        if (!authHeader || !authHeader.startsWith("Bearer ")) {
-            return next(new UnauthorizedException("Authentication token is missing or invalid"));
-        }
-
-        const token = authHeader.split(" ")[1];
-        if (!token) {
+        if (!authHeader) {
             return next(new UnauthorizedException("Authentication token is missing"));
         }
 
-        const decoded = jwt.verify(token, JWT_SECRET) as any;
-        const user = await userModel.findById({ id: decoded._id })
-
-        if (!user) {
-            return next(new UnauthorizedException("User not found"));
-        }
+        const { user } = await decodeToken({ 
+            authorization: authHeader, 
+            tokenType: TokenEnum.access 
+        });
 
         req.user = user as HUserDocument;
         next();
     } catch (error) {
-        return next(new UnauthorizedException("Invalid or expired token"));
+        return next(error);
     }
 };
