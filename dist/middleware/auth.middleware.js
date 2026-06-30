ddcda00986f8448017aacd0251e18cddb4088e99
omar-tarek-abdelfattah <omartarek77124@gmail.com>
2026-06-30 23:29:53 +0300

    jwt is working

--- dist/middleware/auth.middleware.js
+++ dist/middleware/auth.middleware.js
@@ -1,35 +1,23 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.authMiddleware = void 0;
-const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-const User_model_1 = __importDefault(require("../DB/models/User.model"));
 const error_response_1 = require("../utils/response/error.response");
-const user_repository_1 = require("../DB/repositories/user.repository");
-const JWT_SECRET = process.env.JWT_SECRET || "default_random_secret_12345";
-const userModel = new user_repository_1.UserRepository(User_model_1.default);
+const token_security_1 = require("../utils/security/token.security");
 const authMiddleware = async (req, res, next) => {
     try {
         const authHeader = req.headers.authorization;
-        if (!authHeader || !authHeader.startsWith("Bearer ")) {
-            return next(new error_response_1.UnauthorizedException("Authentication token is missing or invalid"));
-        }
-        const token = authHeader.split(" ")[1];
-        if (!token) {
+        if (!authHeader) {
             return next(new error_response_1.UnauthorizedException("Authentication token is missing"));
         }
-        const decoded = jsonwebtoken_1.default.verify(token, JWT_SECRET);
-        const user = await userModel.findById({ id: decoded._id });
-        if (!user) {
-            return next(new error_response_1.UnauthorizedException("User not found"));
-        }
+        const { user } = await (0, token_security_1.decodeToken)({
+            authorization: authHeader,
+            tokenType: token_security_1.TokenEnum.access
+        });
         req.user = user;
         next();
     }
     catch (error) {
-        return next(new error_response_1.UnauthorizedException("Invalid or expired token"));
+        return next(error);
     }
 };
 exports.authMiddleware = authMiddleware;
